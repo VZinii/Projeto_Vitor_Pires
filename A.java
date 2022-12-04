786d972695a4d135c012556bd1f59b3b2847e70d
VZinii <108377833+VZinii@users.noreply.github.com>
2022-12-04 17:28:40 -0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,26 @@
+public class A {
+
+    // Atributos:
+    
+    private int A1;
+
+    private float A2;
+
+    // Métodos:
+
+    public int getA1() {
+        return A1;
+    }
+
+    public float getA2() {
+        return A2;
+    }
+
+    public void MA1(){
+        System.out.println("MA1");
+    }
+
+    public void MA2(){
+        System.out.println("MA2");
+    }
+}
---
+++ B.java
@@ -0,0 +1,27 @@
+public class B {
+
+    // Atributos:
+    
+    private int B1;
+
+    private float B2;
+
+    // Métodos:
+
+    public int getB1() {
+        return B1;
+    }
+
+    public float getB2() {
+        return B2;
+    }
+
+    public void MB1(){
+        System.out.println("MB1");
+    }
+
+    public void MB2(){
+        System.out.println("MB2");
+    }
+
+}
