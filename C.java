6cae0330e692ab9bd06907d7ebbf0b514f8e2a70
Vitor Pires Zini <zini.cdc@gmail.com>
2022-12-04 17:40:52 -0300

    Criação da classe C

---
+++ C.java
@@ -0,0 +1,26 @@
+public class C {
+    
+    // Atributos:
+    
+    private String C1;
+
+    private int C2;
+
+    // Métodos:
+
+    public String getA1() {
+        return C1;
+    }
+
+    public int getA2() {
+        return C2;
+    }
+
+    public void MC1(){
+        System.out.println("MC1");
+    }
+
+    public void MC2(){
+        System.out.println("MC2");
+    }
+}
