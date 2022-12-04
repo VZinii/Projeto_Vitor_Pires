204443eefbb4ad7a5efa4b07cd42c8897ab5203b
Vitor Pires Zini <zini.cdc@gmail.com>
2022-12-04 17:51:26 -0300

    Criando a classe D

---
+++ D.java
@@ -0,0 +1,19 @@
+public class D {
+    
+    // Atributos:
+    
+    private String D1;
+
+    private String D2;
+
+    // Métodos:
+
+    public void MD1(){
+        System.out.println("MD1");
+    }
+
+    public void MD2(){
+        System.out.println("MD2");
+    }
+
+}
